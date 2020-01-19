448d4b75f549ac1fc90fcc48baaff61990412959
Dylan <dyl.crel@gmail.com>
2020-01-19 10:31:07 +0100

    Adding PackageHandler and update gitignore

---
+++ DeepBot.CLI/Network/Packages/PackageAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepBot.CLI.Network.Packages
+{
+    public class PackageAttribute : Attribute
+    {
+        public string Packet;
+
+        public PackageAttribute(string packet) => Packet = packet;
+    }
+}
---
+++ DeepBot.CLI/Network/Packages/PackageReceiver.cs
@@ -0,0 +1,37 @@
+﻿using DeepBot.CLI.Network.Frames;
+using DeepBot.CLI.Network.Tcp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace DeepBot.CLI.Network.Packages
+{
+    public class PackageReceiver
+    {
+        public static readonly List<PackageData> methods = new List<PackageData>();
+
+        public static void Initialize()
+        {
+            Assembly asm = typeof(IFrame).GetTypeInfo().Assembly;
+
+            foreach (var type in asm.GetTypes().SelectMany(x => x.GetMethods()).Where(m => m.GetCustomAttributes(typeof(PackageAttribute), false).Length > 0))
+            {
+                PackageAttribute attribute = type.GetCustomAttributes(typeof(PackageAttribute), true)[0] as PackageAttribute;
+                Type typeValue = Type.GetType(type.DeclaringType.FullName);
+
+                object instance = Activator.CreateInstance(typeValue, null);
+                methods.Add(new PackageData(instance, attribute.Packet, type));
+            }
+        }
+
+        public static void Receive(TcpClient client, string package)
+        {
+            PackageData method = methods.Find(m => package.StartsWith(m.PackageName));
+
+            if (method != null)
+                method.Information.Invoke(method.Instance, new object[2] { client, package });
+        }
+    }
+}
