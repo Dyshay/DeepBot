172471d6c0af7442ecf447e48b1400bff119fa3d
Teucerius <alllstars@hotmail.fr>
2020-01-22 22:00:33 +0100

    Identity Init ,  register/ login page , auth guard , service user ,  implémentation template ,  modif startup

---
+++ DeepBot.Data/Database/RoleDB.cs
@@ -0,0 +1,28 @@
+﻿using AspNetCore.Identity.Mongo.Model;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepBot.Data.Database
+{
+	public class RoleDB : MongoRole
+	{
+		//public ObjectId _id { get; set; }
+
+		public RoleDB()
+		{
+		}
+
+		public RoleDB(string name)
+		{
+			Name = name;
+			NormalizedName = name.ToUpperInvariant();
+		}
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+}
