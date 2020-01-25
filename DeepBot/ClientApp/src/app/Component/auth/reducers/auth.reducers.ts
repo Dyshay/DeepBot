cffe89a04f13b71eaa84b893d9e4783b64261b48
Dylan <dyl.crel@gmail.com>
2020-01-25 10:23:10 +0100

    Adding reducers for auth

---
+++ DeepBot/ClientApp/src/app/Component/auth/reducers/auth.reducers.ts
@@ -0,0 +1,28 @@
+import { createReducer, on } from '@ngrx/store';
+import { AuthActions } from '../actions';
+import { Account } from '../models/account';
+
+export interface State {
+    user: Account | null;
+    error: string | null,
+    pending: boolean,
+}
+
+export const initialState: State = {
+    user: null,
+    error: null,
+    pending: false,
+}
+
+
+export const reducer = createReducer(initialState,
+
+    on(AuthActions.login, (state) => ({ ...state, pending: true })),
+    on(AuthActions.loginSuccess, (state, { user }) => ({ ...state, user, pending: false })),
+    on(AuthActions.loginFailure, (state, { error }) => ({ ...state, error, pending: false }))
+)
+
+
+export const getUser = (state:State) => state.user;
+export const getError = (state: State) => state.error;
+export const getPending = (state: State) => state.pending;
