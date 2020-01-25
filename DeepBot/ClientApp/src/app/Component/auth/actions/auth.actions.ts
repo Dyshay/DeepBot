de5da0ecbcacdb620d420f5d4296e3b8944191d7
Dylan <dyl.crel@gmail.com>
2020-01-25 10:39:01 +0100

    Update missing userModel

--- DeepBot/ClientApp/src/app/Component/auth/actions/auth.actions.ts
+++ DeepBot/ClientApp/src/app/Component/auth/actions/auth.actions.ts
@@ -1,6 +1,7 @@
 import { props, createAction } from '@ngrx/store';
 import { Account } from '../models/account';
+import { User } from 'src/webModel/UserModel';
 
 export const loginSuccess = createAction('[AUTH/LOGIN] Login Success', props<{user: Account}>());
-export const login = createAction('[AUTH/LOGIN] Login');
+export const login = createAction('[AUTH/LOGIN] Login', props<{user: User}>());
 export const loginFailure = createAction('[AUTH/LOGIN] Login Failure', props<{error: any}>());
