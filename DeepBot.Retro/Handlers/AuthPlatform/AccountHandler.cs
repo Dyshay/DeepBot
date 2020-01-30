b1169fa25dfbfc90900096f1b1213c3baaf9ce5b
Dyshay <dyl_crel@hotmail.com>
2020-01-30 17:42:31 +0100

    Global update handler and deeptalker

--- DeepBot.Retro/Handlers/AuthPlatform/AccountHandler.cs
+++ DeepBot.Retro/Handlers/AuthPlatform/AccountHandler.cs
@@ -1,6 +1,7 @@
 ﻿using DeepBot.Core.Hubs;
 using DeepBot.Core.Network;
 using DeepBot.Data.Database;
+using DeepBot.Data.Enums;
 using DeepBot.Data.Model;
 using Microsoft.AspNetCore.SignalR;
 using System;
@@ -12,19 +13,26 @@ namespace DeepBot.Core.Handlers.AuthPlatform
     public class AccountHandler
     {
         [Receiver("HC")]
-        public void GetWelcomeKeyAsync(DeepTalk hub, string package, UserDB account, short tcpId)
+        public void GetWelcomeKeyAsync(DeepTalk hub, string package, AccountDB account, short tcpId)
         {
             //account dispatch value (connecting)
-
-            string welcomeKey = package.Substring(2);
+            account.State = AccountState.CONNECTING;
+            account.WelcomeKey = package.Substring(2);
             hub.Clients.Caller.SendAsync("SendPackage", "1.30", false, tcpId);
 
             // USE THE ACCOUNT AND PASSWORD FROM account
-            hub.Clients.Caller.SendAsync("SendPackage", $"account\n{Hash.EncryptPassword("password", welcomeKey)}", false, tcpId);
+            hub.Clients.Caller.SendAsync("SendPackage", $"${account.Username}\n{Hash.EncryptPassword(account.Password, account.WelcomeKey)}", false, tcpId);
             hub.Clients.Caller.SendAsync("SendPackage", "Af", false, tcpId);
         }
+
+        [Receiver("Ad")]
+        public void GetAccountUserName(DeepTalk hub, string package, AccountDB account, short tcpId) => Console.WriteLine("NEED CALL TO HUB TO FRONT");
+
+        [Receiver("Af")]
+        public void GetLoginQueue(DeepTalk hub, string package, AccountDB account, short tcpId) => Console.WriteLine("NEED CALL TO HUB TO FRONT");
+
         [Receiver("AXK")]
-        public void GetServerWorld(DeepTalk hub, string package, UserDB account, short tcpId)
+        public void GetServerWorld(DeepTalk hub, string package, AccountDB account, short tcpId)
         {
             string gameTicket = package.Substring(14);
             hub.Clients.Caller.SendAsync("NewConnection", Hash.DecryptIp(package.Substring(3, 8)), Hash.DecryptPort(package.Substring(11, 3).ToCharArray()));
