b7b3d258d1ef1ca0f2c41a4d3699f630873a92a9
Dyshay <dyl_crel@hotmail.com>
2020-01-31 12:50:09 +0100

    Using abstract network to dispatch to client

---
+++ DeepBot.Retro/Network/HubMessage/Messages/LogMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DeepBot.Data.Enums;
+
+namespace DeepBot.Core.Network.HubMessage.Messages
+{
+    public class LogMessage : NetworkMessage
+    {
+        public override NetworkType Type => NetworkType.LOG;
+        public LogType LogType { get; set; }
+
+        public LogMessage(LogType type, string message)
+        {
+            Message = message;
+            LogType = type;
+        }
+    }
+}
---
+++ DeepBot.Retro/Network/HubMessage/NetworkMessage.cs
@@ -0,0 +1,14 @@
+﻿using DeepBot.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepBot.Core.Network.HubMessage
+{
+    public abstract class NetworkMessage
+    {
+        public abstract NetworkType Type { get; }
+        public string Message { get; set; }
+
+    }
+}
