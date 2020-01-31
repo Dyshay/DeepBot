114f48639ed2e13eedc49dd87c4433c5ac2f8aad
Dyshay <dyl_crel@hotmail.com>
2020-01-31 09:06:28 +0100

    Add extension and use them

---
+++ DeepBot.Retro/Hubs/DeepTalkExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepBot.Core.Hubs
+{
+    public static class DeepTalkExtension
+    {
+        public static void SendPackage(this DeepTalk hub, string package, short tcpId, bool needResponse = false)
+        {
+            hub.Clients.Caller.SendAsync("SendPackage", package, needResponse, tcpId);
+        }
+    }
+}
