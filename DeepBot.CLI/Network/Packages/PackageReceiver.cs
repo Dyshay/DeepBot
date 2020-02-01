ef8290632c00e55b7e672f1d75cbb1986945f8b5
Dylan <dyl.crel@gmail.com>
2020-02-01 09:27:03 +0100

    remove async

--- DeepBot.CLI/Network/Packages/PackageReceiver.cs
+++ DeepBot.CLI/Network/Packages/PackageReceiver.cs
@@ -4,10 +4,10 @@ namespace DeepBot.CLI.Network.Packages
 {
     public class PackageReceiver
     {
-        public static async void Receive(string package, Account acc, short tcpId)
+        public static void Receive(string package, Account acc, short tcpId)
         {
             System.Console.WriteLine($"[Dofus] {package}");
-            await acc.TalkingService.SendHandlePackageToServer(package, tcpId);
+            acc.TalkingService.SendHandlePackageToServer(package, tcpId).Wait();
         }
     }
 }
