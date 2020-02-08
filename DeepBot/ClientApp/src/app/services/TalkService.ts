59665f39486ba6fcc1f673ba30b8d2593f70b8d9
Dylan <dyl.crel@gmail.com>
2020-02-08 19:06:11 +0100

    update the message model

--- DeepBot/ClientApp/src/app/services/TalkService.ts
+++ DeepBot/ClientApp/src/app/services/TalkService.ts
@@ -6,63 +6,70 @@ import { BotActions } from '../pages/pages/bot/actions';
 
 @Injectable()
 export class TalkService {
-    messageReceived = new EventEmitter<string>();
-    connectionEstablished = new EventEmitter<boolean>();
+  messageReceived = new EventEmitter<string>();
+  connectionEstablished = new EventEmitter<boolean>();
 
-    private connectIsEstablished = false;
-    private _hubConnection: HubConnection;
+  private connectIsEstablished = false;
+  private _hubConnection: HubConnection;
 
-    constructor(private store: Store<fromBot.State>) {
-        this.createConnection();
-        // this.startConnection();
-    }
+  constructor(private store: Store<fromBot.State>) {
+    this.createConnection();
+    // this.startConnection();
+  }
 
-    sendMessage(message: string) {
-        this._hubConnection.invoke('SendPackage', message, '5000');
-    }
+  sendMessage(message: string) {
+    this._hubConnection.invoke('SendPackage', message, '5000');
+  }
 
-    private createConnection(): void {
-        this._hubConnection = new HubConnectionBuilder()
-            .withUrl("https://localhost:44319/deeptalk", {
-                accessTokenFactory: () => {
-                    return localStorage.getItem('DeepBot');
-                }, skipNegotiation: true, transport: HttpTransportType.WebSockets
-            })
-            .build();
-    }
+  private createConnection(): void {
+    this._hubConnection = new HubConnectionBuilder()
+      .withUrl("https://localhost:44319/deeptalk", {
+        accessTokenFactory: () => {
+          return localStorage.getItem('DeepBot');
+        }, skipNegotiation: true, transport: HttpTransportType.WebSockets
+      })
+      .build();
+  }
 
-    createConnexionBot(accountName, accountPassword, serverId, isScan) {
-        console.log(accountName);
-        this._hubConnection.invoke('CreateConnexion', accountName, accountPassword,serverId, isScan);
-    }
+  createConnexionBot(accountName, accountPassword, serverId, isScan) {
+    console.log(accountName);
+    this._hubConnection.invoke('CreateConnexion', accountName, accountPassword, serverId, isScan);
+  }
 
-    public startConnection(): void {
-        this._hubConnection
-            .start()
-            .then(() => {
-                this.connectIsEstablished = true;
-                console.log('Init connection on DeepTalk');
-                this.connectionEstablished.emit(true);
-            })
-            .catch(err => {
-                console.log('Error on initialize connection with DeepTalk, retrying...', err);
-                setTimeout(function () {
-                    this.startConnection();
-                }, 5000);
-            });
-        this.GetClientMessage();
-    }
+  public startConnection(): void {
+    this._hubConnection
+      .start()
+      .then(() => {
+        this.connectIsEstablished = true;
+        console.log('Init connection on DeepTalk');
+        this.connectionEstablished.emit(true);
+      })
+      .catch(err => {
+        console.log('Error on initialize connection with DeepTalk, retrying...', err);
+        setTimeout(function () {
+          this.startConnection();
+        }, 5000);
+      });
+    this.GetClientMessage();
+  }
 
-    private registerOnServerEvents(): void {
-        this._hubConnection.on('ConsoleSend', (data: string) => {
-            this.messageReceived.emit(data);
-        });
-    }
+  private registerOnServerEvents(): void {
+    this._hubConnection.on('ConsoleSend', (data: string) => {
+      this.messageReceived.emit(data);
+    });
+  }
 
-    private GetClientMessage(): void {
-        this._hubConnection.on("DispatchClient", (network, tcpId) => {
-            // if()
-            this.store.dispatch(BotActions.receveidLogs({ network }))
-        })
-    }
+  private GetClientMessage(): void {
+    this._hubConnection.on("DispatchClient", (network, tcpId) => {
+      switch (network.type) {
+        case 0:
+          this.store.dispatch(BotActions.receveidLogs({ network }));
+          break;
+        case 6:
+          this.store.dispatch(BotActions.receveidCharacters({ network }));
+        default:
+          break;
+      }
+    })
+  }
 }
