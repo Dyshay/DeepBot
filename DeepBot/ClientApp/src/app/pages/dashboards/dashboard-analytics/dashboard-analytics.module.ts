90649134b0830a56f107ad8f82dbd7481de45747
Dylan <dyl.crel@gmail.com>
2020-02-16 19:58:18 +0100

    update component, dashboard, widget

--- DeepBot/ClientApp/src/app/pages/dashboards/dashboard-analytics/dashboard-analytics.module.ts
+++ DeepBot/ClientApp/src/app/pages/dashboards/dashboard-analytics/dashboard-analytics.module.ts
@@ -19,6 +19,7 @@ import { BreadcrumbsModule } from '../../../../@vex/components/breadcrumbs/bread
 import { MatButtonModule } from '@angular/material/button';
 import { PageLayoutModule } from '../../../../@vex/components/page-layout/page-layout.module';
 import { ContainerModule } from '../../../../@vex/directives/container/container.module';
+import { MatProgressBarModule } from '@angular/material';
 
 
 @NgModule({
@@ -41,7 +42,8 @@ import { ContainerModule } from '../../../../@vex/directives/container/container
     BreadcrumbsModule,
     MatButtonModule,
     PageLayoutModule,
-    ContainerModule
+    ContainerModule,
+    MatProgressBarModule,
   ]
 })
 export class DashboardAnalyticsModule {
