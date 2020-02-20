b237abd5223a3a987a7531f52996af2d4d86e02e
Dylan <dyl.crel@gmail.com>
2020-02-20 22:39:28 +0100

    first implementation of map

--- DeepBot/ClientApp/src/app/pages/pages/bot-dashboard/map/map.component.ts
+++ DeepBot/ClientApp/src/app/pages/pages/bot-dashboard/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Character } from '../../../../../webModel/Character';
 
@@ -8,7 +8,17 @@ import { Character } from '../../../../../webModel/Character';
     styleUrls: ['./map.component.scss']
 })
 /** map component*/
-export class MapComponent {
+export class MapComponent implements OnInit {
+  ngOnInit(): void {
+    for (let i = 0; i < 17; i++) {
+      this.mapHeight.push(i);
+    }
+    for (let i = 0; i < 15; i++) {
+      this.mapWidth.push(i);    
+    }
+  }
+  mapHeight = [];
+  mapWidth = [];
   @Input() character: Character
     /** map ctor */
   constructor(private translateService: TranslateService) {
