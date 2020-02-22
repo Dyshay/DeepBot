faca395da40cb1bb7760dd504ab4d8622ed0f4ad
Dylan <dyl.crel@gmail.com>
2020-02-22 11:56:32 +0100

    add mapwith

--- DeepBot/ClientApp/src/app/pages/pages/bot-dashboard/map/map.component.ts
+++ DeepBot/ClientApp/src/app/pages/pages/bot-dashboard/map/map.component.ts
@@ -24,9 +24,21 @@ export class MapComponent implements OnInit {
     console.log(`[${i},${x}]`);
   }
 
+  asModulo(i: number){
+    // console.log((i % 2) === 0);
+    return (i % 2) === 0;
+  }
+
   counter(number: Number){
+    // console.log(number)
     return new Array(number)
   }
+
+  counterMapWidth(inputNumber: number, i: number){
+    let val = (i % 2) === 0 ? inputNumber - 1 : inputNumber - 2; 
+    return new Array(val)
+  }
+
   @Input() character: Character
     /** map ctor */
   constructor(private translateService: TranslateService) {
