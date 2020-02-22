7da0fe7da30a80ed20a194ed78e13a323308e8e8
Dylan <dyl.crel@gmail.com>
2020-02-22 10:21:12 +0100

    update map, structur, remove absolute val

--- DeepBot/ClientApp/src/app/pages/pages/bot-dashboard/map/map.component.ts
+++ DeepBot/ClientApp/src/app/pages/pages/bot-dashboard/map/map.component.ts
@@ -10,15 +10,23 @@ import { Character } from '../../../../../webModel/Character';
 /** map component*/
 export class MapComponent implements OnInit {
   ngOnInit(): void {
-    for (let i = 0; i < 17; i++) {
-      this.mapHeight.push(i);
-    }
-    for (let i = 0; i < 15; i++) {
-      this.mapWidth.push(i);    
-    }
+    // for (let i = 0; i < 17; i++) {
+    //   this.mapHeight.push(i);
+    // }
+    // for (let i = 0; i < 15; i++) {
+    //   this.mapWidth.push(i);    
+    // }
   }
-  mapHeight = [];
-  mapWidth = [];
+  mapHeight = 17;
+  mapWidth = 15;
+
+  test(i: Number, x: Number){
+    console.log(`[${i},${x}]`);
+  }
+
+  counter(number: Number){
+    return new Array(number)
+  }
   @Input() character: Character
     /** map ctor */
   constructor(private translateService: TranslateService) {
