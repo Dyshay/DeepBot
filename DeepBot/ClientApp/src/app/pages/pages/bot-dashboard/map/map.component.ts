71b8c11b42b30f7750992dd292fbf36082e01eb3
Dylan <dyl.crel@gmail.com>
2020-03-26 18:25:50 +0100

    update mapComponent

--- DeepBot/ClientApp/src/app/pages/pages/bot-dashboard/map/map.component.ts
+++ DeepBot/ClientApp/src/app/pages/pages/bot-dashboard/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Character } from '../../../../../webModel/Character';
 import $ from 'jquery';
@@ -11,7 +11,8 @@ import { CellTypes } from '../../../../../webModel/Enum/CellTypes';
   styleUrls: ['./map.component.scss']
 })
 /** map component*/
-export class MapComponent implements OnChanges {
+export class MapComponent implements OnChanges, OnInit, OnDestroy {
+
   @Input() map: MapMessage;
   TileWidth = 53; // 43
   TileHeight = 27; // 21.5
@@ -27,7 +28,14 @@ export class MapComponent implements OnChanges {
     Source: { Map: null, Entities: null },
     Canvas: { Map: this.GetTempCanvas(), Entities: this.GetTempCanvas() }
   };
+
   Config = { DisplayCellID: false }
+
+  ngOnInit(){
+    this.Canvas = document.getElementById("isocanvas");
+    this.Context = this.Canvas.getContext('2d');
+    this.Canvas.addEventListener('mousedown', this.handlerMouseClick.bind(this), false);
+  }
 
   ngOnChanges(): void {
     if (this.map !== null) {
@@ -35,15 +43,48 @@ export class MapComponent implements OnChanges {
     }
   }
 
+  ngOnDestroy(){
+    this.Canvas = document.getElementById("isocanvas");
+    this.Context = this.Canvas.getContext('2d');
+    this.Canvas.removeEventListener("mousedown", this.handlerMouseClick.bind(this));
+  }
+
+  handlerMouseClick(event) {
+    var r = this.Canvas.getBoundingClientRect(),
+      x = event.clientX - r.left,
+      y = event.clientY - r.top;
+
+    for (var cellid in this.CELLPOS) {
+      if (this.CELLPOS[cellid].Points !== undefined && this.isInCell(this.CELLPOS[cellid], x, y)) {
+        //SEND CELLID TO SERVER
+        break;
+      }
+    }
+  }
+
+  isInCell(cell, x, y) {
+    var dx = Math.abs(x - cell.Points.centerX);
+    var dy = Math.abs(y - cell.Points.centerY);
+    return (dx / (this.TileWidth) + dy / (this.TileHeight) <= 1.31)
+  }
+
+  getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
+
   // Init cells
   _InitCells(Width, Height, mapData) {
     for (var q = 0; q < this.CellsCount; q++) {
       this.CELLPOS[q] = { los: false };
-      if (mapData.cells[q] !== undefined && !mapData.cells[q].isWalkable) {
-        this.CELLPOS[q].mov = true;
-      }
-      else {
-        this.CELLPOS[q].mov = false;
+      if (mapData.cells[q] !== undefined) {
+        this.CELLPOS[q].mov = !mapData.cells[q].isWalkable
+        this.CELLPOS[q].IsInLineOfSight = mapData.cells[q].isInLineOfSight;
+        this.CELLPOS[q].isInteractiveCell = mapData.cells[q].isInteractiveCell;
+        this.CELLPOS[q].teleport = mapData.cells[q].isTeleportCell
       }
     }
     var cellId = 0;
@@ -63,7 +104,9 @@ export class MapComponent implements OnChanges {
           var top = { x: offsetX + x * cellWidth + midCellWidth, y: offsetY + y * midCellHeight };
           var right = { x: offsetX + x * cellWidth + cellWidth, y: offsetY + y * midCellHeight + midCellHeight };
           var down = { x: offsetX + x * cellWidth + midCellWidth, y: offsetY + y * midCellHeight + cellHeight };
-          this.CELLPOS[cellId++].Points = { left, top, right, down, x, y };
+          var centerX = right.x - ((right.x - left.x) / 2);
+          var centerY = down.y - ((down.y - top.y) / 2);
+          this.CELLPOS[cellId++].Points = { left, top, right, down, x, y, centerX, centerY };
         }
       }
       else {
@@ -72,7 +115,9 @@ export class MapComponent implements OnChanges {
           var top = { x: offsetX + x * cellWidth + cellWidth, y: offsetY + y * midCellHeight };
           var right = { x: offsetX + x * cellWidth + cellWidth + midCellWidth, y: offsetY + y * midCellHeight + midCellHeight };
           var down = { x: offsetX + x * cellWidth + cellWidth, y: offsetY + y * midCellHeight + cellHeight };
-          this.CELLPOS[cellId++].Points = { left, top, right, down, x, y };
+          var centerX = right.x - ((right.x - left.x) / 2);
+          var centerY = down.y - ((down.y - top.y) / 2);
+          this.CELLPOS[cellId++].Points = { left, top, right, down, x, y, centerX, centerY };
         }
       }
     }
@@ -108,12 +153,13 @@ export class MapComponent implements OnChanges {
     let _Canvas = this.GetTempCanvas();
     for (var CellID in this.CELLPOS) {
       if (this.CELLPOS[CellID].Points !== undefined) {
+        let color = this.GetColor(this.CELLPOS[CellID]);
         if (this.CELLPOS[CellID].mov) {
-          this._DrawTileFromPos(_Canvas, this.CELLPOS[CellID].Points, 0xBBBBBB, undefined, false);
+          this._DrawTileFromPos(_Canvas, this.CELLPOS[CellID].Points, color, 0xFFFFFF, false);
         }
         else {
-          this._DrawTileFromPos(_Canvas, this.CELLPOS[CellID].Points, 0xFFFFFF, 0xBBBBBB, true); // Affichage de la grille
-          if (this.Config.DisplayCellID == false) this._DrawTextFromPos(_Canvas, this.CELLPOS[CellID].Points, CellID, "#e74c3c");
+          this._DrawTileFromPos(_Canvas, this.CELLPOS[CellID].Points, color, 0xFFFFFF, true);
+          if (this.Config.DisplayCellID == false) this._DrawTextFromPos(_Canvas, this.CELLPOS[CellID].Points, CellID, "#FFF");
         }
 
       }
@@ -126,6 +172,20 @@ export class MapComponent implements OnChanges {
     this.Storage.Source.Map = null;
     this.Storage.Canvas.Map = this.GetTempCanvas();
     this._Refresh();
+  }
+
+  GetColor(cell) {
+    let color = 0x5A5A8C;
+    if (!cell.mov)
+      color = 0xC1C1D7;
+    if (cell.IsInLineOfSight)
+      color = 0x14141F;
+    if (cell.teleport)
+      color = 0xFFA500;
+    if (cell.isInteractiveCell)
+      color = 0xFFFFFF;
+
+    return color;
   }
 
   // Show & Hide CellID on map
@@ -179,17 +239,17 @@ export class MapComponent implements OnChanges {
       target.strokeStyle = "#" + borderColor.toString(16);
       target.lineWidth = .5;
     }
-    if(isWalkable){
-    target.beginPath();
-    target.moveTo(Points.top.x, Points.top.y + paddingTop);
-    target.lineTo(Points.right.x, Points.right.y + paddingTop);
-    target.lineTo(Points.down.x, Points.down.y + paddingTop);
-    target.lineTo(Points.left.x, Points.left.y + paddingTop);
-    target.closePath();
-    if (color != undefined) target.fill();
-    if (borderColor != undefined) target.stroke();
+    if (isWalkable) {
+      target.beginPath();
+      target.moveTo(Points.top.x, Points.top.y + paddingTop);
+      target.lineTo(Points.right.x, Points.right.y + paddingTop);
+      target.lineTo(Points.down.x, Points.down.y + paddingTop);
+      target.lineTo(Points.left.x, Points.left.y + paddingTop);
+      target.closePath();
+      if (color != undefined) target.fill();
+      if (borderColor != undefined) target.stroke();
     }
-    if(!isWalkable){
+    if (!isWalkable) {
       target.beginPath();
       target.moveTo(Points.left.x, Points.left.y + paddingTop - 8);
       target.lineTo(Points.top.x, Points.top.y + paddingTop - 8);
@@ -245,7 +305,7 @@ export class MapComponent implements OnChanges {
     let target = canvas.getContext("2d");
     target.font = "10px";
     target.fillStyle = _fillStyle;
-    target.fillText(_text, Points.left.x + (paddingTop - (paddingTop / _text.length > 2 ? 9 : 4)), Points.right.y + paddingTop);
+    target.fillText(_text, _text.length > 2 ? Points.centerX - 8 : Points.centerX - 5, Points.centerY + paddingTop + 2.5);
   }
 
   // Get color from string <ElemType> of element
@@ -264,6 +324,7 @@ export class MapComponent implements OnChanges {
     return ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
   }
 
+
   // Get cell coords X/Y on canvas and vice-versa
   GetCoordsFromCellID(CellID) { return { X: CellID % this.MAP_WIDTH, Y: Math.floor(CellID / this.MAP_WIDTH) } }
   GetCellIDFromCoords(x, y) { return null; }
@@ -276,118 +337,12 @@ export class MapComponent implements OnChanges {
     return _Canvas;
   }
 
-  // CommonCellWidth = 43;
-  // CommonCellHeight = 21.5;
-  // CellsCount = 560;
-  // Cells = {};
-  // Context = undefined;
-  // Canvas = undefined;
-
-  // ngOnChanges() {
-  //   this.Canvas = $('#isocanvas');
-  //   this.Context = this.Canvas[0].getContext('2d');
-  //   if (this.map !== null) {
-  //     this.DrawMap(this.Canvas, this.map, this.Context);
-  //   }
-  // }
-
-
-  // resizeCanvas(event) {
-  //   this.Canvas = $('#isocanvas');
-  //   this.Context = this.Canvas[0].getContext('2d');
-  //   this.Context.canvas.width = (event.target.innerWidth - event.target.innerWidth / 2).toString();
-  //   this.Context.canvas.height = (event.target.innerHeight - event.target.innerHeight / 2).toString();
-  //   this.DrawMap(this.Canvas, this.map, this.Context);
-  // }
-
-  // DrawMap(canvas, mapData, Context): void {
-  //   Context = canvas[0].getContext('2d');
-  //   Context.clearRect(0, 0, canvas.width, canvas.height);
-  //   var drawedHeight = Context.canvas.height - (Context.canvas.height / 20) * 2;
-  //   this.BuildMap(Context.canvas.width, drawedHeight, mapData);
-
-  //   for (var c in this.Cells) {
-  //     var cell = this.Cells[c];
-  //     if (cell.mov) {
-  //       Context.fillStyle = 'darkgray';
-  //       Context.font = "10px";
-  //     }
-  //     else if (cell.los) {
-  //       Context.fillStyle = 'black';
-  //     }
-  //     else {
-  //       Context.fillStyle = "#dadada";
-  //     }
-  //     Context.beginPath();
-  //     if (cell.Points !== undefined) {
-  // var paddingTop = cell.Points.down.y - cell.Points.top.y;
-  // Context.moveTo(cell.Points.top.x, cell.Points.top.y + paddingTop);
-  // Context.lineTo(cell.Points.right.x, cell.Points.right.y + paddingTop);
-  // Context.lineTo(cell.Points.down.x, cell.Points.down.y + paddingTop);
-  // Context.lineTo(cell.Points.left.x, cell.Points.left.y + paddingTop);
-  // Context.closePath();
-  // Context.fill();
-  //       let target = canvas[0].getContext("2d");
-  //       target.fillStyle = "#e74c3c";
-  //       target.fillText(c, (cell.Points.x + this.CommonCellWidth / 2) - (3 * cell.length), (cell.Points.y + this.CommonCellHeight / 2) + (cell.length - 1))
-  //     }
-  //   }
-  // }
-
-  // BuildMap(Width, Height, mapData): void {
-  //   for (var q = 0; q < this.CellsCount; q++) {
-  //     this.Cells[q] = { los: false };
-  //     if (mapData.cells[q] !== undefined && !mapData.cells[q].isWalkable) {
-  //       this.Cells[q].mov = true;
-  //     }
-  //     else {
-  //       this.Cells[q].mov = false;
-  //     }
-  //   }
-  //   var cellId = 0;
-  //   var cellWidth = this.GetMaxScaling(Width, Height);
-  //   var cellHeight = Math.ceil(cellWidth / 2);
-
-  //   var offsetX = (Width - ((this.map.mapWidth + 0.5) * cellWidth)) / 2;
-  //   var offsetY = (Height - ((this.map.mapHeight + 0.5) * cellHeight)) / 2;
-
-  //   var midCellHeight = cellHeight / 2;
-  //   var midCellWidth = cellWidth / 2;
-
-  //   for (var y = 0; y <= 2 * (this.map.mapHeight - 1); y++) {
-  //     if (y % 2 === 0)
-  //       for (var x = 0; x <= this.map.mapWidth - 1; x++) {
-  //         var left = { x: offsetX + x * cellWidth, y: offsetY + y * midCellHeight + midCellHeight };
-  //         var top = { x: offsetX + x * cellWidth + midCellWidth, y: offsetY + y * midCellHeight };
-  //         var right = { x: offsetX + x * cellWidth + cellWidth, y: offsetY + y * midCellHeight + midCellHeight };
-  //         var down = { x: offsetX + x * cellWidth + midCellWidth, y: offsetY + y * midCellHeight + cellHeight };
-  //         this.Cells[cellId++].Points = { left, top, right, down, x, y };
-  //       }
-  //     else
-  //       for (var x = 0; x <= this.map.mapWidth - 2; x++) {
-  //         var left = { x: offsetX + x * cellWidth + midCellWidth, y: offsetY + y * midCellHeight + midCellHeight };
-  //         var top = { x: offsetX + x * cellWidth + cellWidth, y: offsetY + y * midCellHeight };
-  //         var right = { x: offsetX + x * cellWidth + cellWidth + midCellWidth, y: offsetY + y * midCellHeight + midCellHeight };
-  //         var down = { x: offsetX + x * cellWidth + cellWidth, y: offsetY + y * midCellHeight + cellHeight };
-  //         this.Cells[cellId++].Points = { left, top, right, down, x, y };
-  //       }
-  //   }
-  // }
-
-  // GetMaxScaling(Width, Height): number {
-  //   var cellWidth = Width / (this.map.mapWidth + 1);
-  //   var cellHeight = Height / (this.map.mapHeight + 1);
-  //   cellWidth = Math.min(cellHeight * 2, cellWidth);
-  //   return cellWidth;
-  // }
   GetMaxScaling(Width, Height): number {
-    var cellWidth = Width / (this.map.mapWidth + 1);
-    var cellHeight = Height / (this.map.mapHeight + 1);
+    var cellWidth = Width / (this.map.mapWidth - 0.8);
+    var cellHeight = Height / (this.map.mapHeight - 0.8);
     cellWidth = Math.min(cellHeight * 2, cellWidth);
     return cellWidth;
   }
-
-
 
   @Input() character: Character
   /** map ctor */
